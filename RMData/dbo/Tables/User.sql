098452b0b022418376c4acfaf7fbaca0fbfa8760
Manpreet <mpsingh17@outlook.co.nz>
2023-10-05 15:08:53 +1300

    Changes in identity (ID) column.

--- RMData/dbo/Tables/User.sql
+++ RMData/dbo/Tables/User.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[User]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [AuthUserId] NVARCHAR(128) NOT NULL, 
+( 
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY,
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL, 
     [EmailAddress] NVARCHAR(256) NOT NULL, 
