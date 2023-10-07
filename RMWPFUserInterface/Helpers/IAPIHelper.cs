ba0853f97ac81a38d38ca9b074bcf84e1f573e76
Manpreet <mpsingh17@outlook.co.nz>
2023-10-07 19:20:33 +1300

    API helper class to call the web API to authenticate the user.

---
+++ RMWPFUserInterface/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using RMWPFUserInterface.Models;
+using System.Threading.Tasks;
+
+namespace RMWPFUserInterface.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
