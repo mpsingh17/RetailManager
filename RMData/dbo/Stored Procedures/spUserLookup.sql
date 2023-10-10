557ad954732bbc8ab6ea684c936e0fc905821fea
Manpreet <mpsingh17@outlook.co.nz>
2023-10-11 10:08:52 +1300

    Store procedure to get user details from DB.

---
+++ RMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128)
+AS
+BEGIN
+	SET NOCOUNT on;
+
+	SELECT Id, FirstName, LastName, EmailAddress, CreatedDate
+	FROM [dbo].[User]
+	WHERE Id = @Id;
+END
+
