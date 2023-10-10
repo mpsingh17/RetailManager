c2fda4325aa8f001d597d65b4c1df536f1f44163
Manpreet <mpsingh17@outlook.co.nz>
2023-10-11 10:10:34 +1300

    Get user data via separate library.
    Our API layer will call methods from this library to talk to DB.

---
+++ RMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,20 @@
+﻿using RMDataManager.Library.Internal.DataAccess;
+using RMDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace RMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string id)
+        {
+            var sql = new SqlDataAccess();
+
+            var p = new { Id = id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RMData");
+
+            return output;
+        }
+    }
+}
